baa87c00a897cd5768d8ffb54499e26a4f0612c9
Jefferson William <jeffersoncomdoisf@gmail.com>
2012-07-16 00:23:59 -0300

    Parallax Mouse 1.1.2 - area mapeada complete

--- libs/jquery.parallax-1.1.2.js
+++ libs/jquery.parallax-1.1.2.js
@@ -48,6 +48,24 @@
             });
         }
 
+        // Mapeando a área de movimento do mouse
+        var map_area = function() {
+            // Obtendo as posições da área
+            area.offsetTop      = area.posy;
+            area.offsetRight    = area.posx + area.width;
+            area.offsetBottom   = area.posy + area.height;
+            area.offsetLeft     = area.posx;
+
+            var horizontal  = (mouse.posx >= area.offsetLeft && mouse.posx <= area.offsetRight) ? true : false;
+            var vertical    = (mouse.posy >= area.offsetTop && mouse.posy <= area.offsetBottom) ? true : false;
+
+            // Verificando se o mouse está sobre a área
+            if(horizontal && vertical)
+                return true;
+            else
+                return false;
+        };
+
         // Configurações do slide show
         var slideshow = {
             init: function() {
@@ -72,46 +90,49 @@
         // Iniciando  o slide show
         slideshow.init();
 
-        return $(area).mousemove( function(e) {
+        return $(document).mousemove( function(e) {
             // Pegando as posições do mouse e jogando na variável global
             mouse.posx = e.pageX;
             mouse.posy = e.pageY;
 
-            // Posição do mouse dentro da área
-            area.mouse_posx = mouse.posx - area.posx;
-            area.mouse_posy = mouse.posy - area.posy;
+            // Se o mouse estiver dentro da área que foi mapeada
+            if( map_area() ) {
+                // Posição do mouse dentro da área
+                area.mouse_posx = mouse.posx - area.posx;
+                area.mouse_posy = mouse.posy - area.posy;
 
-            // Pegar as porcentagens das posições do mouse dentro da área
-            area.mouse_posx_percent = Math.round(area.mouse_posx / area.width * 100);
-            area.mouse_posy_percent = Math.round(area.mouse_posy / area.width * 100);
+                // Pegar as porcentagens das posições do mouse dentro da área
+                area.mouse_posx_percent = Math.round(area.mouse_posx / area.width * 100);
+                area.mouse_posy_percent = Math.round(area.mouse_posy / area.width * 100);
 
-            // Mover a sobra da imagem até encostar na borda da área de movimento do mouse
-            img.each( function() {
-                img.width = $(this).width();
-                img.height = $(this).height();
-                img.posx = $(this).offset().left;
-                img.posy = $(this).offset().top;
-                img.cssLeft = parseInt( $(this).css('left'));
-                img.cssTop = parseInt( $(this).css('top'));
+                // Mover a sobra da imagem até encostar na borda da área de movimento do mouse
+                img.each( function() {
+                    img.width = $(this).width();
+                    img.height = $(this).height();
+                    img.posx = $(this).offset().left;
+                    img.posy = $(this).offset().top;
+                    img.cssLeft = parseInt( $(this).css('left'));
+                    img.cssTop = parseInt( $(this).css('top'));
 
-                // Encontrar a área de sobra da imagem
-                img.width_out = (area.width - img.width) + config.marginHoriz;
-                img.height_out = (area.height - img.height) + config.marginVert;
+                    // Encontrar a área de sobra da imagem
+                    img.width_out = (area.width - img.width) + config.marginHoriz;
+                    img.height_out = (area.height - img.height) + config.marginVert;
 
-                // Quantos % vou mover a imagem de acordo com o movimento do mouse
-                img.move_posx = img.width_out * (area.mouse_posx_percent / 100);
-                img.move_posy = img.height_out * (area.mouse_posy_percent / 50);
+                    // Quantos % vou mover a imagem de acordo com o movimento do mouse
+                    img.move_posx = img.width_out * (area.mouse_posx_percent / 100);
+                    img.move_posy = img.height_out * (area.mouse_posy_percent / 50);
 
-                // Mover a imagem até a posição do mouse levemente
-                $(this).animate({
-                    left: img.move_posx + 'px',
-                    top: img.move_posy + 'px',
-                }, {
-                    duration: 50,
-                    queue: false,
-                    easing: 'linear'
+                    // Mover a imagem até a posição do mouse levemente
+                    $(this).animate({
+                        left: img.move_posx + 'px',
+                        top: img.move_posy + 'px',
+                    }, {
+                        duration: 50,
+                        queue: false,
+                        easing: 'linear'
+                    });
                 });
-            });
+            }
         });
     };
 })(jQuery);
